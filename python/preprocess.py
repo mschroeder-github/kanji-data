29059c97b8f4bf3ef5ccbbe8586505b626b04fec
Markus Schroeder <markus.schroeder@dfki.de>
2024-08-26 09:02:43 +0200

    improved anki generation: one card per kanji

--- python/preprocess.py
+++ python/preprocess.py
@@ -963,6 +963,186 @@ def make_anki(romaji_reading=False, separator=" ", meaning_kanji_bg="#fffee6", r
     output_file = f'../anki/Kyouiku-Kanji-Deutsch_{reading_mode}.apkg'
     package.write_to_file(output_file)
 
+
+def make_anki_v2(romaji_reading=False, separator=" "):
+    with open('../kanji-kyouiku-de-radicals-array-mnemonics-wip.json', 'rt', encoding='utf-8') as file:
+        kanji_kyouiku = json.load(file)
+
+    with open('card.css', 'rt') as file:
+        css = file.read().strip()
+
+    qfmt_default = '''
+<span class="kanji">{{Kanji}}</span>
+<br/>
+<br/>
+{{type:Antwort}}
+    '''
+
+    afmt_postfix = '''
+    <br/>
+    <br/>
+    <span class="additional additional_de">{{Bedeutung_Weitere_Deutsch}}</span><br/>
+    <span class="additional additional_en">{{Bedeutung_Weitere_Englisch}}</span>
+    '''
+
+    afmt_mnemonic = ('{{FrontSide}}' +
+                     '<br/><br/>{{Merksatz_Bedeutung}}<br/><br/>{{Merksatz_Lesung}}' +
+                     afmt_postfix)
+
+    afmt_image = ('{{FrontSide}}' +
+                  '<br/><br/>{{Merksatz_Lesung}}<br/><br/><div class="img_container">{{Merkbild_Bedeutung}}{{Merkbild_Kanji}}</div>' +
+                  afmt_postfix)
+
+    # two models
+    # * meaning, reading -> mnemonic, mnemonic
+    # * meaning, reading -> image
+
+    mnemonic_meaning = genanki.Model(
+        1695632296,
+        'Kyōiku-Kanji Deutsch Bedeutung/Lesung Merksatz',
+        fields=[
+            {'name': 'Kanji'},
+            {'name': 'Antwort'},
+            {'name': 'Bedeutung'},
+            {'name': 'Bedeutung_Weitere_Deutsch'},
+            {'name': 'Bedeutung_Weitere_Englisch'},
+            {'name': 'Merksatz_Bedeutung'},
+            {'name': 'Lesung_Hiragana'},
+            {'name': 'Lesung_Romaji'},
+            {'name': 'Merksatz_Lesung'}
+        ],
+        templates=[
+            {
+                'name': 'Bedeutung/Lesung Merksatz Karte',
+                'qfmt': qfmt_default,
+                'afmt': afmt_mnemonic
+            }
+        ],
+        css=css
+    )
+
+    image_meaning = genanki.Model(
+        1835909438,
+        'Kyōiku-Kanji Deutsch Bedeutung/Lesung Merkbild',
+        fields=[
+            {'name': 'Kanji'},
+            {'name': 'Antwort'},
+            {'name': 'Bedeutung'},
+            {'name': 'Bedeutung_Weitere_Deutsch'},
+            {'name': 'Bedeutung_Weitere_Englisch'},
+            {'name': 'Merkbild_Bedeutung'},
+            {'name': 'Merkbild_Kanji'},
+            {'name': 'Lesung_Hiragana'},
+            {'name': 'Lesung_Romaji'},
+            {'name': 'Merksatz_Lesung'}
+        ],
+        templates=[
+            {
+                'name': 'Bedeutung/Lesung Merkbild Karte',
+                'qfmt': qfmt_default,
+                'afmt': afmt_image
+            }
+        ],
+        css=css
+    )
+
+    decks = []
+
+    # main deck
+    kanji_kyouiku_deck = genanki.Deck(
+        1818403339,
+        'Kyōiku-Kanji Deutsch')
+    decks.append(kanji_kyouiku_deck)
+
+    # Create a subdeck
+    no2grade = { }
+    no2grade['1'] = genanki.Deck(
+        1519507022,
+        'Kyōiku-Kanji Deutsch::1. Schuljahr')
+    no2grade['2'] = genanki.Deck(
+        2002120349,
+        'Kyōiku-Kanji Deutsch::2. Schuljahr')
+    no2grade['3'] = genanki.Deck(
+        1621021171,
+        'Kyōiku-Kanji Deutsch::3. Schuljahr')
+    no2grade['4'] = genanki.Deck(
+        1298392758,
+        'Kyōiku-Kanji Deutsch::4. Schuljahr')
+    no2grade['5'] = genanki.Deck(
+        1670745543,
+        'Kyōiku-Kanji Deutsch::5. Schuljahr')
+    no2grade['6'] = genanki.Deck(
+        1555589751,
+        'Kyōiku-Kanji Deutsch::6. Schuljahr')
+
+    for entry in kanji_kyouiku:
+        if not is_done(entry):
+            continue
+
+        # turn reading mnemonic to reading check
+        reading_strs = get_reading_strs(entry)
+
+        reading_hiragana = separator.join([r[0] for r in reading_strs])
+        reading_romaji = separator.join([r[1] for r in reading_strs])
+        reading_answer = reading_romaji if romaji_reading else reading_hiragana
+
+        answer = separator.join([entry['meanings_de'][0].lower(), reading_answer])
+
+        kanji_guid = genanki.guid_for(entry['kanji'])
+
+        if not entry['is_radical']:
+            note = genanki.Note(
+                model=mnemonic_meaning,
+                guid=kanji_guid,
+                fields=[
+                    entry['kanji'],
+                    answer,
+                    entry['meanings_de'][0].lower(),
+                    ", ".join(entry['meanings_de']),
+                    ", ".join(entry['meanings']),
+                    entry['mnemonic_meaning_de'],
+                    reading_hiragana,
+                    reading_romaji,
+                    entry['mnemonic_reading_de']
+                ]
+            )
+
+        else:
+            radical_name = entry['wk_radicals_de'][0]
+            note = genanki.Note(
+                model=image_meaning,
+                guid=kanji_guid,
+                fields=[
+                    entry['kanji'],
+                    answer,
+                    entry['meanings_de'][0].lower(),
+                    ", ".join(entry['meanings_de']),
+                    ", ".join(entry['meanings']),
+                    f'<img class="image" src="{radical_name}-img.jpg">',
+                    f'<img class="image" src="{radical_name}-kanji.png">',
+                    reading_hiragana,
+                    reading_romaji,
+                    entry['mnemonic_reading_de']
+                ]
+            )
+
+        no2grade[str(entry['grade'])].add_note(note)
+
+    for no, deck in no2grade.items():
+        if len(deck.notes) > 0:
+            decks.append(deck)
+
+    # export package
+    package = genanki.Package(decks)
+    package.media_files = []
+    package.media_files.extend(glob.glob('../img/*.jpg'))
+    package.media_files.extend(glob.glob('../img/*.png'))
+
+    reading_mode = "Romaji" if romaji_reading else "Hiragana"
+    output_file = f'../anki/Kyouiku-Kanji-Deutsch_{reading_mode}.apkg'
+    package.write_to_file(output_file)
+
+
 def get_reading_strs(entry):
     soup = BeautifulSoup(entry['mnemonic_reading_de'], 'html.parser')
     readings = soup.find_all("span", class_="reading")
@@ -979,12 +1159,12 @@ def get_reading_strs(entry):
 # dict_to_array()
 # prepare_mnemonics()
 # check_radical_kanji_mapping()
-#reading_decision()
-#best_reading_german_word_match()
-#radical_graph()
-#special_radicals()
+# reading_decision()
+# best_reading_german_word_match()
+# radical_graph()
+# special_radicals()
 # germanet_categories()
 # order()
 
-make_anki(romaji_reading=True)
-make_anki(romaji_reading=False)
+make_anki_v2(romaji_reading=True)
+make_anki_v2(romaji_reading=False)
